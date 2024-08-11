d051b3ae5a9b9a79ee02ae5504bd743554ffafa7
Ravichandran <ravichandranshc@gmail.com>
2024-08-11 20:04:31 +0530

    Final Out put for longest substrig for custom data algorithum

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+//Longest Substring find from defined strings
+
 function longestsubString(str) {
-    console.log(str.substring(0,4));
-    
-    const arr = [];
-    for (let index = 0; index < str.length; index++) {
-        for (let index1 = index; index1 < str.length; index1++) {
-            if(str[index] === str[index1]) {
-                arr.push(str.substring(index, index1));
-            }
-            
-        }        
+  //console.log(str.substring(0, 4));
+  const arr = [];
+  for (let index = 0; index < str.length; index++) {    
+    for (let index1 = (index); index1 < str.length; index1++) {          
+        const inserString = str.substring(index, (index1));      
+        if((inserString.includes(str[index1]))) {            
+            arr.push(inserString);
+            break;            
+        }  
     }
-    return arr;
+  }
+  return arr;
 }
-const test = "ravichandran";
+const test = "bbbbsdsiiesds";
+// const test = "Ravichandran";
 console.log(longestsubString(test));
 const charArr = longestsubString(test);
 let maxCharaterCount = 0;
-let maxCharacter = '';
+let maxCharacter = "";
 for (let index = 0; index < charArr.length; index++) {
-    if(charArr[index].length >= maxCharaterCount) {
-        maxCharaterCount = charArr[index].length;
-        maxCharacter = charArr[index];
-    }    
+  if (charArr[index].length >= maxCharaterCount) {
+    maxCharaterCount = charArr[index].length;
+    maxCharacter = charArr[index];
+  }
 }
 console.log(maxCharacter);
-
