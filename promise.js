954b3048e2d87b7a39c885747488f25eb8e771e8
Ravichandran <ravichandranshc@gmail.com>
2024-08-19 20:50:49 +0530

    promise all settle scenarious

--- promise.js
+++ promise.js
@@ -4,11 +4,11 @@ const promise3 = Promise.reject("Third");
 const promise4 = Promise.resolve("Fourth");
 
 const runPromises = async () => {
-  const res1 = await Promise.all([promise1, promise2]);
-  const res2 = await Promise.all([promise3, promise4]);
+  const res1 = await Promise.allSettled([promise1, promise2]);
+  const res2 = await Promise.allSettled([promise3, promise4]);
   return [res1, res2];
 };
 
 runPromises()
-  .then((res) => console.log(res))
+  .then((res) => console.log([...res].flat(2)))
   .catch((err) => console.log(err));
