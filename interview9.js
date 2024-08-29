5a3a405fcf97c517dc0f2b39769b74dbf2204167
Ravichandran <ravichandranshc@gmail.com>
2024-08-29 16:35:13 +0530

    interviewqus

---
+++ interview9.js
@@ -0,0 +1,46 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// function test(str) {
+//     let count = 0;
+//     return function() {
+//         console.log("Parent param Val "+(++count))
+//     }
+// }
+
+// const reFun = test("Ravi");
+// reFun();
+// reFun();
+// reFun();
+// reFun();
+// reFun();
+
+// let arr = [2,5,6,7,1]
+// const target = 7
+
+// for(let i = 0; i < arr.length; i++) {
+//     for(let j = i; j < arr.length; j++) {
+//         if(((arr[i] + arr[j]) === target)) {
+//             console.log(`[${arr[i]}, ${arr[j]}]`)
+//         }else if((arr[i] == target)){
+//             console.log(`[${arr[i]}]`)
+//         }
+//     }
+//     // arr.filter((data, index) => {
+//     //     return arr[i] + arr[]
+//     // }))
+//     // if(arr.length >= i && ((arr[i] + arr[i + 1]) == target)){
+//     //     console.log(`${arr[i]} ${arr[i + 1]}`)
+//     // }
+// }
+
+const arr = [];
+arr[0] = 3;
+arr.push(4);
+console.log(arr);
+ 
+const obj = {};
+obj.key = "abcd";
+obj["value"] = "234"
+console.log(obj)
+ 
