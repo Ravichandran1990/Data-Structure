d43973ab631da77e55e92f1373b346f5b15bd30b
Ravichandran <ravichandranshc@gmail.com>
2024-09-06 10:45:12 +0530

    interview Question

---
+++ interview_15.js
@@ -0,0 +1,42 @@
+let arr = [1, 2, [3, 5, [3, 5, 6]], 5, 6, [6, 7]];
+
+const arr_1 = [];
+function customeArr(arrData) {
+  for (let i = 0; i < arrData.length; i++) {
+    if (typeof arrData[i] === "object") {
+      customeArr(arrData[i]);
+    } else {
+      arr_1.push(arrData[i]);
+    }
+  }
+}
+
+customeArr(arr);
+console.log(arr_1);
+let dublicate = [];
+const arr_2 = arr_1.reduce((pre, curr) => {
+  if (pre.indexOf(curr) === -1) {
+    pre.push(curr);
+  } else {
+    dublicate.push(curr);
+  }
+  return pre;
+}, []);
+console.log(dublicate);
+
+let obj = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 3,
+    e: {
+      f: 1,
+      d: 4,
+    },
+  },
+};
+let obj_1 = JSON.parse(JSON.stringify(obj));
+
+obj_1.c.d = 5;
+console.log(obj);
+console.log(obj_1);
