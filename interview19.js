021688b3a9541a6b6f9a097868265f8f76c4fd5e
Ravichandran <ravichandranshc@gmail.com>
2024-09-17 11:59:40 +0530

    interview Que

---
+++ interview19.js
@@ -0,0 +1,17 @@
+const arr1 = [1,2,3];
+
+const arr2 = [1,2,3];
+
+const str = "1,2,3";
+ 
+console.log(arr1 == str);
+
+console.log(arr1 == arr2);
+ 
+let x = 3;
+
+let y="5";
+ 
+console.log(x+ +y)
+// console.log(x+y, x+ +y, x-y)
+ 
